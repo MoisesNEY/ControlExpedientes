b7baf1fbac231a3db9f45b718884a52ceecf1cda
Moises Ney <moisesney2@gmail.com>
2026-02-22 16:40:57 -0600

    feat(backend): add session auth filter, update SecurityConfig, enable CORS and session config

--- backend/src/main/java/ni/edu/mney/web/rest/AuthenticationResource.java
+++ backend/src/main/java/ni/edu/mney/web/rest/AuthenticationResource.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
