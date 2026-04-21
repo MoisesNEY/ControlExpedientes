b7af6cb2099f52f1e71a50574eb3a9d4b13ff030
Moises Ney <moisesney2@gmail.com>
2026-04-21 16:37:46 -0600

    feat: add KeycloakAdmin properties and update Liquibase seed script

--- backend/src/main/java/ni/edu/mney/config/ApplicationProperties.java
+++ backend/src/main/java/ni/edu/mney/config/ApplicationProperties.java
@@ -12,11 +12,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class ApplicationProperties {
 
     private final Liquibase liquibase = new Liquibase();
+    private final KeycloakAdmin keycloakAdmin = new KeycloakAdmin();
 
     // jhipster-needle-application-properties-property
 
     public Liquibase getLiquibase() {
         return liquibase;
+    }
+
+    public KeycloakAdmin getKeycloakAdmin() {
+        return keycloakAdmin;
     }
 
     // jhipster-needle-application-properties-property-getter
@@ -34,4 +39,34 @@ public class ApplicationProperties {
         }
     }
     // jhipster-needle-application-properties-property-class
+    public static class KeycloakAdmin {
+
+        private String clientId;
+        private String username;
+        private String password;
+
+        public String getClientId() {
+            return clientId;
+        }
+
+        public void setClientId(String clientId) {
+            this.clientId = clientId;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
 }
